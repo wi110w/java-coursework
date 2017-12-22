c82c2f52ecbb4a12b14f3474f320ffec3768856e
Eugenia Glinska <willow@nyan.org.ua>
2017-12-22 21:22:18 +0200

    Add save function for saving diagram in PNG format

--- src/scatter_editor/Main.java
+++ src/scatter_editor/Main.java
@@ -1,14 +1,24 @@
 package scatter_editor;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.SnapshotResult;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
 public class Main extends Application {
@@ -32,6 +42,7 @@ public class Main extends Application {
         Button editData = new Button("Edit data series");
         Button deleteData = new Button("Delete data series");
         Button editTicks = new Button("Edit ticks");
+        Button saveDiagram = new Button("Save diagram");
 
         setNames.setOnAction(actionEvent -> {
             Names names = new Names();
@@ -75,13 +86,32 @@ public class Main extends Application {
             editTickUnit.start(new Stage());
         });
 
-        root.add(setNames, 1,1);
-        root.add(editNames, 1, 2);
-        root.add(deleteNames, 1,3);
-        root.add(newData, 2, 1);
-        root.add(editData,2,2);
-        root.add(deleteData, 2, 3);
-        root.add(editTicks, 3, 1);
+        saveDiagram.setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Save diagram");
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("PNG files (*.png)", "*.png"),
+                    new FileChooser.ExtensionFilter("All Files", "*.*")
+            );
+
+            File file = fileChooser.showSaveDialog(scene.getWindow());
+            if(file == null) {
+                return;
+            }
+
+            sc.snapshot(snapshotResult -> {
+                save(snapshotResult, file);
+                return null; }, new SnapshotParameters(), null);
+        });
+
+        root.add(setNames, 0,1);
+        root.add(editNames, 0, 2);
+        root.add(deleteNames, 0,3);
+        root.add(newData, 1, 1);
+        root.add(editData,1,2);
+        root.add(deleteData, 1, 3);
+        root.add(editTicks, 2, 1);
+        root.add(saveDiagram, 2,2);
         root.add(sc, 0,0);
         GridPane.setColumnSpan(sc, 4);
         GridPane.setHalignment(sc, HPos.CENTER);GridPane.setMargin(sc, new Insets(0, 0,25,0));
@@ -95,4 +125,14 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+        private void save(SnapshotResult snapshotResult, File f) {
+            WritableImage image = snapshotResult.getImage();
+            BufferedImage bufferedImage = SwingFXUtils.fromFXImage(image, null);
+            try {
+                ImageIO.write(bufferedImage, "png", f);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 }
